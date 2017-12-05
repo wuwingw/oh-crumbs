0aad9a11e0fa9c43b1b9807089bfc81229ff3d29
W <quackoquack22@gmail.com>
2017-12-05 23:36:33 +0000

    Initial commit

---
+++ js/game.js
@@ -0,0 +1,114 @@
+var TopDownGame = TopDownGame || {};
+
+TopDownGame.Game = function(){};
+
+TopDownGame.Game.prototype = {
+	PLAYER_SPEED: 100,
+
+
+	create: function() {
+
+		// TILEMAP
+
+		this.map = this.game.add.tilemap('mine'); // create tilemap from json
+		this.map.addTilesetImage('tiles', 'gameTiles'); // add its tileset image
+
+		this.backgroundLayer = this.map.createLayer('backgroundLayer');
+		this.blockedLayer = this.map.createLayer('blockedLayer');
+
+		this.map.setCollisionBetween(1, 1000, true, 'blockedLayer');
+
+		this.backgroundLayer.resizeWorld(); // make game world same size as map
+
+		// ITEMS
+
+		this.createItems();
+
+		// PLAYER
+
+		var result = this.findObjectsByType('playerStart', this.map, 'objectsLayer');
+		this.player = this.game.add.sprite(result[0].x, result[0].y, 'player');
+		this.game.physics.arcade.enable(this.player);
+
+		this.game.camera.follow(this.player);
+
+		this.cursors = this.game.input.keyboard.createCursorKeys();
+
+		// FOG
+
+		this.fog = this.game.add.sprite(0, 0, 'fog');
+		// this.fog.fixedToCamera = true;
+		// this.fog.scale.setTo(0.5);
+		this.fog.anchor.setTo(0.5);
+	},
+
+	update: function() {
+
+		// INPUT
+
+		this.player.body.velocity.y = 0;
+		this.player.body.velocity.x = 0;
+
+		if(this.cursors.up.isDown) {
+			this.player.body.velocity.y -= this.PLAYER_SPEED;
+		}
+		else if(this.cursors.down.isDown) {
+			this.player.body.velocity.y += this.PLAYER_SPEED;
+		}
+		if(this.cursors.left.isDown) {
+			this.player.body.velocity.x -= this.PLAYER_SPEED;
+		}
+		else if(this.cursors.right.isDown) {
+			this.player.body.velocity.x += this.PLAYER_SPEED;
+		}
+
+		// FOG
+		this.fog.x = this.player.x;
+		this.fog.y = this.player.y;
+
+		// COLLISION
+
+		this.game.physics.arcade.collide(this.player, this.blockedLayer);
+		this.game.physics.arcade.overlap(this.player, this.items, this.collect, null, this);
+
+	},
+
+	createItems: function() {
+		this.items = this.game.add.group();
+		this.items.enableBody = true;
+
+		var item;
+		result = this.findObjectsByType('treasure', this.map, 'objectsLayer');
+		result.forEach(function(element) {
+			this.createFromTiledObject(element, this.items);
+		}, this);
+	},
+
+	// find objects in a Tiled layer that contain a property called "type" equal to a certain value
+	findObjectsByType: function(type, map, layer) {
+		var result = new Array();
+
+		map.objects[layer].forEach(function(element){
+			if(element.properties.type === type) {
+				//Phaser uses top left, Tiled bottom left so we have to adjust the y position
+				element.y -= map.tileHeight;
+				result.push(element);
+			}      
+		});
+
+		return result;
+	},
+
+	// create a sprite from an object
+	createFromTiledObject: function(element, group) {
+		var sprite = group.create(element.x, element.y, element.properties.sprite);
+
+		Object.keys(element.properties).forEach(function(key) {
+			sprite[key] = element.properties[key];
+		});
+	},
+
+	collect: function(player, item) {
+		item.destroy();
+	}
+}
---
+++ js/preload.js
@@ -0,0 +1,29 @@
+var TopDownGame = TopDownGame || {};
+ 
+//loading the game assets
+TopDownGame.Preload = function(){};
+ 
+TopDownGame.Preload.prototype = {
+  preload: function() {
+    //show loading screen
+    this.preloadBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY + 128, 'preloadbar');
+    this.preloadBar.anchor.setTo(0.5);
+ 
+    this.load.setPreloadSprite(this.preloadBar);
+ 
+    //load game assets
+    this.load.tilemap('level1', 'assets/tilemaps/level1.json', null, Phaser.Tilemap.TILED_JSON);
+    this.load.tilemap('mine', 'assets/tilemaps/mine.json', null, Phaser.Tilemap.TILED_JSON);
+    this.load.image('gameTiles', 'assets/images/tiles.png');
+    this.load.image('greencup', 'assets/images/greencup.png');
+    this.load.image('bluecup', 'assets/images/bluecup.png');
+    this.load.image('player', 'assets/images/player.png');
+    this.load.image('browndoor', 'assets/images/browndoor.png');
+
+    this.load.image('fog', 'assets/images/fog.png')
+    
+  },
+  create: function() {
+    this.state.start('Game');
+  }
+};
