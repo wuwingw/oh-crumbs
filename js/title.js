bc09d4cf825796f6da5a54eb8bad1e12e3db08ce
W <quackoquack22@gmail.com>
2017-12-07 01:34:33 +0000

    Fiddling with title screen

--- js/title.js
+++ js/title.js
@@ -5,14 +5,17 @@ TopDownGame.Title = function(){};
  
 TopDownGame.Title.prototype = {
   create: function() {
-      this.titleText = this.game.add.bitmapText(this.game.width/2, this.game.height/2 - 16, 'pixeled', 'O H _ C R U M B S', 6);
+      this.titleText = this.game.add.bitmapText(this.game.width/2, this.game.height/2 - 16, 'pixeled', 'O H   C R U M B S', 6);
       this.titleText.anchor.setTo(0.5);
 
       this.subtitleText = this.game.add.bitmapText(this.game.width/2, this.game.height/2 + 16, 'pixeled', 'hit space', 6);
       this.subtitleText.anchor.setTo(0.5);
 
-      this.crumb = this.game.add.sprite(this.game.width/2, this.game.height/2, 'crumb');
+      this.crumb = this.game.add.sprite(this.game.width/2 - 24, this.game.height/2 - 16, 'crumb');
       this.crumb.anchor.setTo(0.5);
+
+      // this.treasure = this.game.add.sprite(this.game.width/2, this.game.height/2, 'mummy');
+      // this.treasure.anchor.setTo(0.5);
   },
 
   update: function() {
